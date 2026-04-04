9a047d074e00ca7a0631cd1345b24951152772bb
oreste <muhirwa.g.oreste@gmail.com>
2026-04-04 02:13:40 +0200

    Add `kotlinx-coroutines-core` dependency to the `composeApp` module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
             implementation(compose.materialIconsExtended)
+            implementation(libs.kotlinx.coroutines.core)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)

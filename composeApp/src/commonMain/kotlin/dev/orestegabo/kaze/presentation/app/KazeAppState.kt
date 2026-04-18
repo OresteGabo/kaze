1357ac663e747fdda358a2b05e44a62b1e409a1a
oreste <muhirwa.g.oreste@gmail.com>
2026-04-19 01:12:49 +0200

    Add `themeMode` to `KazeAppUiState` to support theme selection
    This change introduces a `themeMode` property to the application's UI state, defaulting to `KazeThemeMode.SYSTEM`.

--- composeApp/src/commonMain/kotlin/dev/orestegabo/kaze/presentation/app/KazeAppState.kt
+++ composeApp/src/commonMain/kotlin/dev/orestegabo/kaze/presentation/app/KazeAppState.kt
@@ -2,11 +2,13 @@ package dev.orestegabo.kaze.presentation.app
 
 import dev.orestegabo.kaze.presentation.demo.KazeDestination
 import dev.orestegabo.kaze.presentation.navigation.MapNavigationTarget
+import dev.orestegabo.kaze.theme.KazeThemeMode
 
 internal data class KazeAppUiState(
     val isReady: Boolean = false,
     val isOnboardingVisible: Boolean = false,
     val onboardingPage: Int = 0,
+    val themeMode: KazeThemeMode = KazeThemeMode.SYSTEM,
     val currentDestination: KazeDestination = KazeDestination.HOME,
     val activeMapTarget: MapNavigationTarget = MapNavigationTarget(),
     val feedbackMessage: String = "",

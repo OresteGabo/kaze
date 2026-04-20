88beba3022f2eaa7bf332e14b9c09b0b27ce21c5
oreste <muhirwa.g.oreste@gmail.com>
2026-04-20 02:31:08 +0200

    Update `ContentView` to handle authentication deep links in the iOS application
    This change integrates deep link support into the iOS entry point by:
    - Adding an `onOpenURL` modifier to the `ComposeView`.
    - Passing incoming URLs to `handleKazeAuthDeepLink` to manage authentication flows.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -14,8 +14,10 @@ struct ContentView: View {
     var body: some View {
         ComposeView()
             .ignoresSafeArea()
+            .onOpenURL { url in
+                AuthDeepLinksKt.handleKazeAuthDeepLink(url: url.absoluteString)
+            }
     }
 }
-
 
 

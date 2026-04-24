7b208d061dd670f67b5991d7ceb0bad9ffec46a5
oreste <muhirwa.g.oreste@gmail.com>
2026-04-24 21:42:44 +0200

    Add `listHotels` method to `HotelRepository`
    This commit updates the `HotelRepository` interface to support fetching a collection of hotels:
    - Add a new `listHotels` suspend function that returns a list of `Hotel` objects.

--- shared/src/commonMain/kotlin/dev/orestegabo/kaze/data/repository/HotelRepository.kt
+++ shared/src/commonMain/kotlin/dev/orestegabo/kaze/data/repository/HotelRepository.kt
@@ -3,6 +3,7 @@ package dev.orestegabo.kaze.data.repository
 import dev.orestegabo.kaze.domain.Hotel
 
 interface HotelRepository {
+    suspend fun listHotels(): List<Hotel>
     suspend fun getHotel(hotelId: String): Hotel?
     suspend fun requireHotel(hotelId: String): Hotel
 }
